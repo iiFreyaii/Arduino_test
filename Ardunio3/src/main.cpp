20268b7e0f94eb7d5403a185c97d3d4b9c8dffbc
Freyaaaaa <167000763+iiFreyaii@users.noreply.github.com>
2024-07-04 16:00:00 +0800

    7.4

--- Ardunio3/src/main.cpp
+++ Ardunio3/src/main.cpp
@@ -1,20 +1,26 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);//rs, en, d4, d5, d6, d7
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // rs, en, d4, d5, d6, d7
 int contrast = 6;
+int counter = 0;
 
-void setup() {
-  pinMode(contrast, OUTPUT); // 配置 引脚模式为输出模式
-  analogWrite(contrast,140);
-  lcd.begin(16, 2); //   初始化lcd ，并定义LCD1602的规格
- //   lcd 显示字符串
-  lcd.setCursor(1,0); 
-  lcd.print("^---^   ^---^");
-  lcd.setCursor(0,1); 
-  lcd.print("(*&~&*) ('g^g')");
+void setup()
+{
+  Serial.begin(9600);
+  // pinMode(contrast, OUTPUT);
+  // analogWrite(contrast, 150);
+  lcd.begin(16, 2);
+  // lcd.setCursor(0, 0);
+  // lcd.print("HEllO WORLD");
 }
-//（排针焊反了，线乱乱的、、、、、、、
-void loop() {
- 
+
+void loop()
+{
+  lcd.clear();
+  Serial.println(String(counter));
+  lcd.setCursor(0, 1);
+  lcd.print(String(counter));
+  counter++;
+  delay(800);
 }
